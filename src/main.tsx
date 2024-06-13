25c7849718524bfb3f37a460d847ba8b6782d5ff
Hamzat Victor <oluwaborihamzat@gmail.com>
2024-06-13 23:48:59 +0100

    feat: hook up react query

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,31 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { AxiosError, isAxiosError } from "axios";
+import { Toaster, toast } from "sonner";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const queryClient = new QueryClient({
+  defaultOptions: {
+    mutations: {
+      onError(error) {
+        if (isAxiosError(error)) {
+          // Extract error message from axios error
+          const errMessage = (error as AxiosError<{ message: string }>).response
+            ?.data.message;
+          toast.error(errMessage, {});
+        }
+      },
+    },
+  },
+});
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <Toaster richColors />
+      <App />
+    </QueryClientProvider>
+  </React.StrictMode>
+);
