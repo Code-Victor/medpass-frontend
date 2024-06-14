473b607da4a43c184347e5602ada6f40cc90d2d2
Hamzat Victor <oluwaborihamzat@gmail.com>
2024-06-14 23:26:03 +0100

    feat: add react-router

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { AxiosError, isAxiosError } from "axios";
 import { Toaster, toast } from "sonner";
-
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./pages/index.tsx";
 const queryClient = new QueryClient({
   defaultOptions: {
     mutations: {
@@ -20,12 +20,22 @@ const queryClient = new QueryClient({
     },
   },
 });
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "about",
+    element: <div>About</div>,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <Toaster richColors />
-      <App />
+      <RouterProvider router={router} />
     </QueryClientProvider>
   </React.StrictMode>
 );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
-function App() {
+function Home() {
   return <h1 className="text-lg font-blue-500">hello</h1>;
 }
 
-export default App;
+export default Home;
