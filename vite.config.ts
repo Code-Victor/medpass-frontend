4e23869881c0a2626ca695865631a7832e346c8e
Hamzat Victor <oluwaborihamzat@gmail.com>
2024-06-14 23:33:07 +0100

    feat: add PWA support

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { VitePWA } from 'vite-plugin-pwa'
 
 function pathResolve(dir: string) {
   return path.join(__dirname, ".", dir);
 }
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA({ registerType: "autoUpdate" })],
   resolve: {
     alias: [
       {
