aa7b76417c9d05e3ad3203cd314fd1db35e8c3ed
Hamzat Victor <oluwaborihamzat@gmail.com>
2024-06-18 00:19:19 +0100

    feat: complete department page

--- src/routes/admin/_adminauth/department.lazy.tsx
+++ src/routes/admin/_adminauth/department.lazy.tsx
@@ -1,15 +1,144 @@
 import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 import { createLazyFileRoute } from "@tanstack/react-router";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Textarea } from "@/components/ui/textarea";
 
+const addDepartmentSchema = z.object({
+  departmentName: z.string().min(1, "Department name is required"),
+  departmentEmail: z.string().email("Invalid email address").optional(),
+  hodName: z.string().min(1, "Head of department name is required"),
+  hodEmail: z.string().email("Invalid email address"),
+  descriptiion: z.string().min(1, "Description is required"),
+});
+type AddDepartmentSchema = z.infer<typeof addDepartmentSchema>;
 export const Route = createLazyFileRoute("/admin/_adminauth/department")({
-  component: () => (
+  component: Department,
+});
+
+function Department() {
+  const addDepartmentForm = useForm<AddDepartmentSchema>({
+    resolver: zodResolver(addDepartmentSchema),
+  });
+  const onSubmit = (data: AddDepartmentSchema) => {
+    console.log(data);
+  };
+  return (
     <main className="max-w-5xl mx-auto px-4">
       <div className="flex justify-between items-center">
         <div className="flex flex-col gap-1">
           <h1 className="font-bold text-2xl">Departments</h1>
           <p className="text-gray-11">Manage departments here</p>
         </div>
-        <Button>Add departments</Button>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button>Add departments</Button>
+          </DialogTrigger>
+          <DialogContent className="sm:max-w-[580px]">
+            <DialogHeader>
+              <DialogTitle>Add Department</DialogTitle>
+              <DialogDescription>
+                Add a department in your hospital and send an invite to the head
+                of that department.
+              </DialogDescription>
+            </DialogHeader>
+            <Form {...addDepartmentForm}>
+              <form
+                className="grid gap-4 py-4"
+                onSubmit={addDepartmentForm.handleSubmit(onSubmit)}
+              >
+                <FormField
+                  control={addDepartmentForm.control}
+                  name="departmentName"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Department Name</FormLabel>
+                      <FormControl>
+                        <Input placeholder="dentistry" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={addDepartmentForm.control}
+                  name="departmentEmail"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Department Email(Optional)</FormLabel>
+                      <FormControl>
+                        <Input placeholder="hello@medpass.com" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={addDepartmentForm.control}
+                  name="descriptiion"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Description</FormLabel>
+                      <FormControl>
+                        <Textarea placeholder="..." {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={addDepartmentForm.control}
+                  name="hodName"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>HOD's Name</FormLabel>
+                      <FormControl>
+                        <Input placeholder="john kent" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={addDepartmentForm.control}
+                  name="hodEmail"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>HOD's Email</FormLabel>
+                      <FormControl>
+                        <Input placeholder="hello@medpass.com" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </form>
+            </Form>
+            <DialogFooter>
+              <Button type="submit">Save changes</Button>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
       </div>
       <div className="mt-8">
         {/* if empty */}
@@ -25,9 +154,8 @@ export const Route = createLazyFileRoute("/admin/_adminauth/department")({
         </div>
       </div>
     </main>
-  ),
-});
-
+  );
+}
 function EmptyDepartmentList() {
   return (
     <div className="flex px-6 py-8 shadow-md rounded-lg bg-white">
